c49d421b2d06c667a7e09b9a00d5172d8c31f904
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2017-07-22 17:43:05 +0800

    script for packaging CARTA on ubuntu

---
+++ final_ubuntu_packaging_steps.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+###
+### Final packaging steps on ubuntu    (Last updated: 22/7/17)
+###
+### Uses our own modified 'carta-distro' template (taken from the NRAO casa-pkg)
+### (Including the 'desktop' to 'CARTA' name update from the 28th April 2017
+### develop branch commit)
+###
+### Note: Some paths may need to be adjusted for your system. 
+###       So please check every line carefully.
+###
+
+# 0. Define the installed location of your Qt 5.3 and CARTA source code (for grabing the latest html):
+CARTABUILDHOME=~/cartahome
+qtpath=/opt/Qt/5.3/gcc_64                               ## location of your Qt installation
+casapath=~/cartahome/CARTAvis-externals/ThirdParty/casa ## location of casacore/code installation
+cartapath=~/cartahome/CARTAvis                          ## location of your CARTA source code
+thirdparty=~/cartahome/CARTAvis-externals/ThirdParty    ## location where qooxdoo is installed 
+packagepath=/tmp/carta
+version=1.1.1                                           ## version number to be put on the final package
+
+# 1. Export the location of the successfully built casacore/casacode library files
+export LD_LIBRARY_PATH=$casapath/trunk/linux/lib
+
+
+# 2. Download and run the latest make-app-carta script using the 'carta-distro' template
+svn export https://github.com/CARTAvis/deploytask/trunk/make-app-carta
+chmod 755 make-app-carta
+rm -rf $packagepath-$version
+svn export https://github.com/CARTAvis/deploytask/trunk/carta-distro
+./make-app-carta -ni -v version=$version out=/tmp ws=$CARTABUILDHOME/CARTAvis/build/cpp/desktop template=carta-distro
+
+
+# 3. Fix a few things
+cp -r $qtpath/plugins/platforms $packagepath-$version/bin/
+
+cp $qtpath/lib/libQt5DBus.so.5 $packagepath-$version/lib/
+
+chrpath -c -r '$ORIGIN/../../lib:$ORIGIN/../../plugins/CasaImageLoader' $packagepath-$version/plugins/ImageStatistics/libplugin.so
+
+cp $thirdparty/cfitsio/lib/libcfitsio.so.5 $packagepath-$version/lib/
+
+rm -f $packagepath-$version/include/python2.7
+
+# 4. Copy over the html and qooxdoo
+cp -r $cartapath/carta/html5 $packagepath-$version/etc
+rm  $packagepath-$version/etc/html5/common/qooxdoo-3.5.1-sdk
+cp -r $thirdparty/qooxdoo-3.5.1-sdk $packagepath-$version/etc/html5/common/qooxdoo-3.5.1-sdk
+
+rm -f $packagepath-$version/etc/html5/html5.iml
+rm -f $packagepath-$version/etc/html5/._html5.iml
+rm -f $packagepath-$version/etc/html5/.idea
+
+# 5. Setup geodetic and ephemerides data
+curl -O -L http://www.asiaa.sinica.edu.tw/~ajm/carta/measures_data.tar.gz
+tar -xvf measures_data.tar.gz
+mv measures_data $packagepath-$version/etc/
+rm measures_data.tar.gz
+
+
+# 6. Copy over the sample images
+curl -O -L http://www.asiaa.sinica.edu.tw/~ajm/carta/images.tar.gz
+tar -xvf images.tar.gz
+mkdir $packagepath-$version/etc/images
+mv images $packagepath-$version/etc/
+rm images.tar.gz
+
+
+# 7. Fix for QtSql; copy the library file to the correct location
+mkdir $packagepath-$version/bin/sqldrivers
+cp $qtpath/plugins/sqldrivers/libqsqlite.so $packagepath-$version/bin/sqldrivers
+
+
+#Finished
+

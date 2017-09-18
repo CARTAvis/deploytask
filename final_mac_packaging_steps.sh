135ec7c428d3dd764c691e9fc4a84d0e3d915713
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2017-09-18 08:14:15 +0800

    Testing fancy dmg installer 2

--- final_mac_packaging_steps.sh
+++ final_mac_packaging_steps.sh
@@ -183,5 +183,5 @@ mv /tmp/Carta.app /tmp/$newappname.app
 # 14. Download and run the dmg creation script
 #curl -O -L https://raw.githubusercontent.com/CARTAvis/deploytask/fromCASAPackagingRepo/packaging/scripts/make-carta-dmg.sh
 curl -O -L https://raw.githubusercontent.com/CARTAvis/deploytask/fromCASAPackagingRepo/packaging/scripts/make-carta-dmg-release.sh
-chmod 755 make-carta-dmg.sh
+chmod 755 make-carta-dmg-release.sh
 ./make-carta-dmg.sh /tmp/$newappname.app

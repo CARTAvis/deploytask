0359570dd24e0df3144a04826d1fbecf8cfac97d
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2017-09-20 11:01:04 +0800

    Stopping sample images loading by default

--- Carta.app/Contents/MacOS/carta.sh
+++ Carta.app/Contents/MacOS/carta.sh
@@ -21,12 +21,11 @@ dirname=$PWD/$dirname
 fi
 
 logfilename=$HOME/.cartavis/log/$(date +"%Y%m%d_%H%M%S_%Z").log
-imagefile=$HOME/CARTA/Images/aJ.fits
 
 
-if [ ! -d $HOME/CARTA/Images/CubesTest -o\
-     ! -d $HOME/.cartavis/log  -o\
+if [ ! -d $HOME/.cartavis/log  -o\
      ! -f $HOME/.cartavis/config.json -o\
+     ! -d $HOME/CARTA/cache  -o\
      ! -d $HOME/CARTA/snapshots/data ]; then
     $dirname/setupcartavis.sh 2>&1  > /dev/null
 fi
@@ -34,5 +33,5 @@ fi
 export CASARCFILES=$dirname/casarc
 
 cd $dirname
-./$appname --html $dirname/../Resources/html/desktop/desktopIndex.html --scriptPort 9999 $imagefile >> $logfilename   2>&1 &
+./$appname --html $dirname/../Resources/html/desktop/desktopIndex.html --scriptPort 9999 >> $logfilename   2>&1 &
 #./$appname --html $dirname/VFS/DesktopDevel/desktop/desktopIndex.html --scriptPort 9999 $imagefile 2>&1 &
